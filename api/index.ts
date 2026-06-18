46dc5c7cc29653fe07c22a3e19f6c600abff0dd3
Admin_Ganteng <115489699+bankyan80@users.noreply.github.com>
2026-06-18 17:39:27 +0700

    test: only jwt import

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
-import { verifySession } from '../lib/session';
+import jwt from 'jsonwebtoken';
 
 export default function handler(_req: any, res: any) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ session: typeof verifySession }));
+  const k = Object.keys(jwt).join(',');
+  res.end(JSON.stringify({ jwt: 'ok', keys: k }));
 }
